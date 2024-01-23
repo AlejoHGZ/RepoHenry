1f9c1cf75658a7b70150ed45a2e263a2e1cec193
Juan Carlos Gutierrez <DiosJuanVero2015@gmail.com>
2024-01-23 12:38:20 -0300

    Realizando Ejercicios FuncionesJS

--- M05 Fundamentos Javascript/ejercicios/07.js
+++ M05 Fundamentos Javascript/ejercicios/07.js
@@ -1,8 +1,43 @@
-function esTipoDato(valor) {
+function esTipoDato(num1,num2) {
   // La función recibe un argumento "valor".
   // Retorna el tipo de dato de este valor.
   // Por ejemplo: "string", "number", "boolean", "object", etc.
   // Tu código:
+
+    let resultado; 
+    resultado = num1 + num2; 
+    return resultado;
 }
 
-module.exports = esTipoDato;
+var suma = esTipoDato(5,2);
+console.log(suma);
+
+
+function palabras(palabra1,palabra2){ // dentro de los parentesis, van los parametros a recibir 
+   var nomCompleto; // luego declaramos alguna variable para utilizar 
+   nomCompleto = palabra1 + palabra2;  // utilizamo la variable y asignamos que se va a realizar con los parametros a recibir 
+  return nomCompleto;
+}
+
+var nombreCompleto = palabras("Alejo ","Gutierrez");
+console.log(nombreCompleto);
+
+
+function sumar(numero1,numero2,nombre){
+  
+  
+  var sumaTot;
+  sumaTot= numero1+numero2+nombre;
+  return sumaTot;
+}
+
+var sumatoria = sumar(10,11," años de edad");
+console.log(sumatoria);
+
+
+function name (first,second){
+  return first+second;
+}
+
+var nombreCom = name("hola ","Gutierrez","como estas");
+console.log(nombreCom);
--- M05 Fundamentos Javascript/ejercicios/08.js
+++ M05 Fundamentos Javascript/ejercicios/08.js
@@ -3,6 +3,13 @@ function esNumeroEntero(numero) {
   // Verifica si este es un número entero o no.
   // Returna true si lo es, de lo contrario, retorna false.
   // Tu código:
+
+  return Number.isInteger(numero) && numero % 1 === 0; 
 }
 
-module.exports = esNumeroEntero;
+var miNumero = 42.5; 
+var esEntero = esNumeroEntero(miNumero); 
+
+console.log(esEntero);
+//Esta función utiliza Number.isInteger() para verificar si el número es
+//un entero y también verifica que su parte decimal sea cero para asegurarse de que no sea un número decimal. El resultado de la función se almacena en la variable esEntero, que será true si el número es un entero y false si no lo es.
